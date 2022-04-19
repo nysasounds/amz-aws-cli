4d42c90928569ffb9bdc9cef010ebd49123042da
jcat <jcat@nysasounds.co.uk>
2022-04-19 20:21:38 +0100

    Use funtion for bash completion, passing through COMP_LINE and COMP_POINT variables

--- utils/aws_completion.bash
+++ utils/aws_completion.bash
@@ -1,1 +1,4 @@
-complete -C "${SNAP}/bin/aws_completer" amz-aws-cli.amz-aws-cli amz-aws-cli
+_amz_aws_cli_complete() {
+    COMPREPLY=( $( COMP_LINE="$COMP_LINE" COMP_POINT="$COMP_POINT" "${SNAP}/bin/aws_completer" ) )
+}
+complete -F _amz_aws_cli_complete aws amz-aws-cli amz-aws-cli.aws amz-aws-cli.amz-aws-cli
